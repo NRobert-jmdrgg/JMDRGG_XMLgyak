e689584790b276c12f2714114b7fae6d861b063b
sm730bf <mesziist@gmail.com>
2022-11-02 12:44:30 +0100

    dom parser

--- jmdrgg_1102/DomParseJmdrgg/src/domjmdrgg/DomReadJmdrgg.java
+++ jmdrgg_1102/DomParseJmdrgg/src/domjmdrgg/DomReadJmdrgg.java
@@ -1,7 +1,41 @@
 package domjmdrgg;
 
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.*;
+import java.io.*;
+
 public class DomReadJmdrgg {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+  public static void main(String[] args) throws SAXException, IOException, ParserConfigurationException {
+    try {
+      File inputFile = new File("usersJmdrgg.xml");
+      DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+      DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+      Document doc = dBuilder.parse(inputFile);
+      doc.getDocumentElement().normalize();
+      System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+      NodeList nList = doc.getElementsByTagName("users");
+
+      for (int i = 0; i < nList.getLength(); i++) {
+        Node nNode = nList.item(i);
+        System.out.println("\nCurrent Element :" + nNode.getNodeName());
+
+        if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+          Element elem = (Element) nNode;
+          String uid = elem.getElementsByTagName("uid").item(i).getTextContent();
+          String fname = elem.getElementsByTagName("firstname").item(i).getTextContent();
+          String lname = elem.getElementsByTagName("lastname").item(i).getTextContent();
+          String pname = elem.getElementsByTagName("profession").item(i).getTextContent();
+
+          System.out.println("uid: " + uid);
+          System.out.println("firstname" + fname);
+          System.out.println("lastname" + lname);
+          System.out.println("profession" + pname);
+        }
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+  }
 }
